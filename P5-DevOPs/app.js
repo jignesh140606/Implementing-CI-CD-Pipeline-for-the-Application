2289a2f1da1f13eb7f96e243cf5d6fff8ddb6b5b
Jignesh Ambaliya <23it002@charusat.edu.in>
2025-10-05 14:20:12 +0530

    Update app.js

--- P5-DevOPs/app.js
+++ P5-DevOPs/app.js
@@ -3,10 +3,11 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Jignesh Ambaliya the revised CI/CD pipeline!');
+  res.send('Hello from Jignesh Ambaliya - 23IT002 the revised CI/CD pipeline!');
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 
 });
+
